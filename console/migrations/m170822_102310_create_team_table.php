bd78ae4f13aa87532e0720ab585de6aaffc86558
s-nadesh <nadesh@arkinfotec.com>
2017-08-22 16:16:13 +0530

    Sumanas - Team table create

---
+++ console/migrations/m170822_102310_create_team_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `team`.
+ */
+class m170822_102310_create_team_table extends Migration {
+
+    /**
+     * @inheritdoc
+     */
+    public function up() {
+        $this->createTable('{{%team}}', [
+            'team_id' => $this->primaryKey(),
+            'name' => $this->string(64)->notNull(),
+            'designation' => $this->string(32)->notNull(),
+            'hobbies' => $this->text()->notNull(),
+            'description' => $this->text(),
+            'image' => $this->string(256),
+            'created_at' => $this->datetime()->notNull(),
+            'updated_at' => $this->datetime(),
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down() {
+        $this->dropTable('{{%team}}');
+    }
+
+}
